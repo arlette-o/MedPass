c2c82d53909a155c3ed67a3d9634a32089e3f34b
Arlette Olalde <aolaldeflores@gmail.com>
2025-01-23 13:31:20 -0800

    created header/navbar

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component, signal } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-header',
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
+})
+export class HeaderComponent {
+  title = 'HealthyU';
+  subtitle = signal('ur mom');
+}
